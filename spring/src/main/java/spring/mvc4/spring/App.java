48961197bfd2b849eeae25f104802c45d8d9b6d2
Faheem Akhtar <mohammad.akhtar@snapdeal.com>
2016-05-13 12:13:39 +0530

    addind a spring and hacker rank problems solution project

---
+++ spring/src/main/java/spring/mvc4/spring/App.java
@@ -0,0 +1,13 @@
+package spring.mvc4.spring;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
